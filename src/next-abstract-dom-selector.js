1a4ccc03d510e7676b47e9a11d2e124deee7d29a
afeiship <afeiship@gmail.com>
2019-02-12 22:50:19 +0800

    feat: add qsa for dom-selector

--- src/next-abstract-dom-selector.js
+++ src/next-abstract-dom-selector.js
@@ -1,8 +1,15 @@
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
+  var nxStubSingleton = nx.stubSingleton || require('next-stub-singleton');
 
   var NxAbstractDomSelector = nx.declare('nx.AbstractDomSelector', {
+    statics: nx.mix(nxStubSingleton(), {
+      qsa: function(inSelector, inContext) {
+        nx.mix(this.instance, { selector: inSelector, context: inContext });
+        return this.instance.select();
+      }
+    }),
     methods: {
       init: function(inSelector, inContext) {
         this.selector = inSelector;
